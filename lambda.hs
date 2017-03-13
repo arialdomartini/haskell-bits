db7c07979fcf0e1fd5bb1b29efbd3c17617fdda2
Arialdo Martini <arialdomartini@gmail.com>
2017-03-13 07:00:35 +0100

    Using lambdas as anonymous functions

--- lambda.hs
+++ lambda.hs
@@ -11,3 +11,10 @@ const2 :: a -> (b -> a)
 const2 = \a -> (\_ -> a)
 
 const3 x = \_ -> x
+
+
+odds n = map f [0..n-1]
+         where
+           f x = x*2 + 1
+
+oddsl n = map (\x -> x*2 + 1) [0..n-1]
