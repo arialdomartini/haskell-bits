1a7c4912557e120a82aa885a1144c1fefa8a6bb8
Arialdo Martini <arialdomartini@gmail.com>
2017-04-03 07:48:05 +0200

    Halve function, version 2

--- ex_3.hs
+++ ex_3.hs
@@ -1,4 +1,6 @@
 halve1 xs = (take n xs, drop n xs)
   where n = length xs `div` 2
 
-main = print(halve1 [1, 2, 3, 4, 5, 6])
+halve2 xs = splitAt (length xs `div` 2) xs
+
+main = print(halve2 [1, 2, 3, 4, 5, 6])
