d9425318198e858912b91707095c2c4eac64d7fd
Arialdo Martini <arialdo.martini@gmail.com>
2024-05-27 19:10:05 +0200

    Tree is an instance of Functor

--- tests/BookOfMonads/Chapter2StateMonadSpec.hs
+++ tests/BookOfMonads/Chapter2StateMonadSpec.hs
@@ -9,9 +9,12 @@ countLeaves :: Tree v -> Int
 countLeaves (Leaf _)     = 1
 countLeaves (Node l r)   = countLeaves l + countLeaves r
 
-convert :: (String->Int) ->Tree String -> Tree Int
+convert :: (a -> b) -> Tree a -> Tree b
 convert f (Leaf v)     = Leaf (f v)
 convert f (Node l r) = Node (convert f l) (convert f r)
+
+instance Functor Tree where
+  fmap = convert
 
 spec :: Spec
 spec = do
@@ -23,6 +26,6 @@ spec = do
   it "maps leaves" $ do
     let treeOfWords = Node (Leaf "one") (Node (Leaf "two") (Leaf "three"))
         treeOfNumbs = Node (Leaf 3) (Node (Leaf 3) (Leaf 5 ))
-        treeOfLengths = convert length treeOfWords
+        treeOfLengths = length <$> treeOfWords
     treeOfLengths `shouldBe` treeOfNumbs
 
