35ff7f2d9ebd7f4e4b8ae25d3ffc86f76ed205de
Arialdo Martini <arialdo.martini@gmail.com>
2024-05-27 18:57:41 +0200

    A leaf has a content

--- tests/BookOfMonads/Chapter2StateMonadSpec.hs
+++ tests/BookOfMonads/Chapter2StateMonadSpec.hs
@@ -2,17 +2,17 @@ module BookOfMonads.Chapter2StateMonadSpec(spec) where
 
 import Test.Hspec
 
-data Tree = Leaf | Node Tree Tree
+data Tree v = Leaf v | Node (Tree v)  (Tree v)
   deriving (Show, Eq)
 
-countLeaves :: Tree -> Int
-countLeaves Leaf        = 1
+countLeaves :: Tree v -> Int
+countLeaves (Leaf _)     = 1
 countLeaves (Node l r) = countLeaves l + countLeaves r
 
 spec :: Spec
 spec = do
   it "counts the number of leaves" $ do
-    let tree = Node Leaf (Node Leaf Leaf)
+    let tree = Node (Leaf ()) (Node (Leaf ()) (Leaf ()) )
         numberOfLeaves = countLeaves tree
     numberOfLeaves `shouldBe` 3
 
