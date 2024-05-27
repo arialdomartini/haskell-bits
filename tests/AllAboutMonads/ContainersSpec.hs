80d6c80e489932e16d800b5515778f32b8034ede
Arialdo Martini <arialdo.martini@gmail.com>
2024-05-27 18:32:53 +0200

    newtype to offer a different implementation of a type class

---
+++ tests/AllAboutMonads/ContainersSpec.hs
@@ -0,0 +1,35 @@
+module AllAboutMonads.ContainersSpec(spec) where
+
+import Test.Hspec
+
+newtype AppendLastList a = AppendLastList [a] deriving (Eq, Show)
+
+class Container c where
+  empty :: c a
+  insert :: c a -> a -> c a
+
+instance Container [] where
+  empty = []
+  insert xs x = x:xs
+
+instance Container AppendLastList where
+  empty = AppendLastList []
+  insert xs x =
+    let (AppendLastList elements) = xs
+    in AppendLastList (elements ++ [x])
+
+spec :: Spec
+spec = do
+  it "defines an empty List" $ do
+    let xs = empty  :: [Int]
+    xs `shouldBe` []
+
+
+  it "append to the head" $ do
+    let xs = (empty `insert` "foo") `insert` "bar" in
+      xs `shouldBe` ["bar", "foo"]
+
+
+  it "append lasts" $ do
+    let xs = (empty `insert` "foo") `insert` "bar" :: AppendLastList String in
+      xs `shouldBe` AppendLastList ["foo", "bar"]
