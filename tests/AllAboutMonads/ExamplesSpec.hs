0795b6cf9c77375f1f956f0574262db6997f3a00
Arialdo Martini <arialdo.martini@gmail.com>
2024-05-27 17:02:22 +0200

    All combinations using liftM2

--- tests/AllAboutMonads/ExamplesSpec.hs
+++ tests/AllAboutMonads/ExamplesSpec.hs
@@ -19,6 +19,13 @@ getName :: String -> Maybe String
 getName name = do let db = [("John", "Smith, John"), ("Mike", "Caine, Michael")]
                   liftM swapNames (lookup name db)
 
+
+allCombinations :: (a->a->a) -> [a] -> [a]
+allCombinations op xs =
+  let lift = liftM2 op in
+      lift xs xs
+
+      
 spec :: Spec
 spec = do
   it "swaps names" $ do
@@ -30,3 +37,5 @@ spec = do
   it "swap names" $ do
     liftM fixName [("John", "Smith, John"), ("Mike", "Caine, Michael")] `shouldBe` [("John", "John Smith"), ("Mike", "Michael Caine")]
 
+  it "all combinations" $ do
+    allCombinations (+) [1,2,3] `shouldBe` [1+1,1+2,1+3,  2+1,2+2,2+3, 3+1,3+2,3+3]
